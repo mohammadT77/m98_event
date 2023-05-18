ac39d19f37e10ead78785584a7c89dbeb6221c24
matehrani <t.mohammadamin@gmail.com>
2023-05-18 08:01:58 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+
+
+
+
+def main():
+    print("Maktab 98 Events")
+    print("Description\n")
+
+    print("Items:")
+    print("1. Events")
+    print("2. Ticket management")
+
+    while True:
+
+        user_choice = int(input("Select an item (0 to exit): "))
+        if user_choice==0:
+            exit(0)
+        elif user_choice==1:
+            
+
+            break
+        elif user_choice==2:
+            break
+            # tickets
+        else:
+            print("Invalid choice, try again")
+
+
+
