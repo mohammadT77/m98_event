4489611fbd26cbc6a8db099a17fe568969a2f865
matehrani <t.mohammadamin@gmail.com>
2023-05-18 10:03:56 +0200

    main with fileSystemNode

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
-from menu.models import Node
+from menu.models import FileSystemNode
 
-n1 = Node('n1')
-n2 = Node('n2', parent=n1)
+n1 = FileSystemNode("Maktab folder")
+n2 = FileSystemNode("HW1", parent=n1)
 
-n3 = Node('n3',parent=n1)
-n4 = Node('n4', parent=n2)
-nodes = [n1,n2,n3,n4]
+n3 = FileSystemNode("Final project", parent=n1)
+n4 = FileSystemNode("main.py",parent=n2)
 
-children = lambda node: filter(lambda n: n.parent == node, nodes)
-is_leaf = lambda node: not list(children(node))  # [] -> True , [n1] -> False
-print(is_leaf(n1))
+n1.print_tree()
