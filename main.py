457dac8a6a3442306a9e6348e83f9354c72b0f51
matehrani <t.mohammadamin@gmail.com>
2023-05-19 13:49:45 +0200

    imlp models ticket

--- main.py
+++ main.py
@@ -35,15 +35,21 @@
 # root_menu = generate_menu_from_dict(main_menu_dict, parent=None)
 # root_menu.print_tree()
 
-from ticketing.models import Event
+from ticketing.models import *
 from datetime import datetime
 # e1 = Event(1, "asd", 10, datetime.now())
 
 # e1.save_file()
 
-with open('1.event', 'r') as f:
-    e1 = Event.from_file_content(f.read())
+# with open('1.event', 'r') as f:
+#     e1 = Event.from_file_content(f.read())
 
-print(e1)
-print(e1.title)
-print(e1.event_date)
+# print(e1)
+# print(e1.title)
+# print(e1.event_date)
+# print(Event.get_all_path())
+# print(*Event.get_all())
+e1 = Event(1,'a', 1, datetime.now())
+t1 = Ticket(1, e1, '1234')
+t1.save_file()
+print(t1)
--- ticketing/models.py
+++ ticketing/models.py
@@ -1,11 +1,48 @@
 from datetime import datetime
-# from dataclasses import dataclass
+from dataclasses import dataclass
+import os
+from abc import ABC, abstractclassmethod, abstractmethod
 
 
-class ManagerUser:
+class BaseModel(ABC):
+
+    def save_file(self):
+        with open(self.get_file_path(), 'wt') as f:
+            f.write(self.get_file_content())
+
+    @abstractmethod
+    def get_file_path(self):
+        pass
+
+    @abstractmethod
+    def get_file_content(self) -> str:
+        pass
+
+    @abstractclassmethod
+    def from_str(cls, s: str):
+        pass
+
+    @classmethod
+    def from_path(cls, path: str):
+        with open(path, 'r') as f:
+            return cls.from_str(f.read())
+
+    @abstractclassmethod
+    def get_all_path(cls) -> list[str]:
+        pass
+
+    @classmethod
+    def get_all(cls):
+        return map(cls.from_path, cls.get_all_path())
+
+
+    def delete(self):
+        os.remove(self.get_file_path())
+
+class ManagerUser(BaseModel):
     pass
 
-class Event:
+class Event(BaseModel):
     id: int
     title: str
     capacity: int
@@ -17,28 +54,64 @@ class Event:
         self.title = title
         self.capacity = capacity
         self.event_date = event_date
-
     
-    def save_file(self):
-        with open(self.get_file_path(), 'wt') as f:
-            f.write(self.get_file_content())
-
     def get_file_path(self):
         return f'{self.id}.event'
-
-
+    
     def get_file_content(self) -> str:
         return f"{self.id}||{self.title}||{self.capacity}||{self.event_date}"
-
+    
     @classmethod
-    def from_file_content(cls, s: str):
-        # s = 1||akbar||10||20222.22.2.2.2.
-        # [1, akbar, 10, 22....]
+    def from_str(cls, s: str):
         id, title, cap, event_date = s.split('||')
         return cls(int(id), title, int(cap), datetime.fromisoformat(event_date))
-
-
     
+    @classmethod
+    def get_all_path(cls) -> list[str]:
+        return [path for path in os.listdir() if path.endswith('.event')]
+    
+    @classmethod
+    def find_by_id(cls, id):
+        for e in cls.get_all():
+            if e.id == id:
+                return e
+        assert None, "Not found" # AssertionError
 
-class Ticket:
-    pass
+    def __repr__(self) -> str:
+        return f"<Event #{self.id}>"
+
+    def __str__(self):
+        return f"Event #{self.id}: {self.title} - {self.event_date}"
+
+
+@dataclass
+class Ticket(BaseModel):
+    id: int  # for behanam
+    event: Event
+    customer_id: str
+    quantity: int = 1
+
+    def get_file_path(self):
+        return f'{self.id}.ticket'
+    
+    def get_file_content(self) -> str:
+        return f"{self.id}:{self.event.id}:{self.customer_id}:{self.quantity}"
+    
+    @classmethod
+    def from_str(cls, s: str):
+        id, event_id, customer_id, quantity = s.split(':')
+        event = Event.find_by_id(event_id)
+        return cls(int(id), event, customer_id, int(quantity))
+    
+    @classmethod
+    def get_all_path(cls) -> list[str]:
+        return [path for path in os.listdir() if path.endswith('.ticket')]
+    
+    @classmethod
+    def find_by_id(cls, id):
+        for e in cls.get_all():
+            if e.id == id:
+                return e
+        assert None, "Not found" # AssertionError
+
+
