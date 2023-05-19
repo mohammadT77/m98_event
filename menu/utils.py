19655df4323fc8fb16c9029f75c327b2eafb3f35
matehrani <t.mohammadamin@gmail.com>
2023-05-19 08:41:45 +0200

    add exit interrupt

--- menu/utils.py
+++ menu/utils.py
@@ -4,9 +4,12 @@ def get_input(prompt, retry=True, target_type=str):
     """
     assert callable(target_type)
 
-    user_input = input(prompt)
     try:
+        user_input = input(prompt)  # if Ctrl+C -> raise KeyboardInterrupt
         user_input = target_type(user_input)
+    except KeyboardInterrupt:
+        print("\nForce Exit...")
+        exit(0)
     except Exception as err:
         if retry:
             print("Invalid input, try again")
@@ -14,37 +17,3 @@ def get_input(prompt, retry=True, target_type=str):
         else:
             raise TypeError("Invalid input, try again") from err  # NewException -> err
     return user_input
-
-
-"""
-Example dictionary:
-
-main_menu_dict = {
-    'name':'Maktab events',
-    'children':[
-        {
-            'name':'Events',
-            'description':"See all events",
-            'children':[
-                
-            ]
-        },
-        {
-            'name': 'Tickets',
-            'description':"See all Tickets",
-            'children':[
-                {
-                    'name':'Buy ticket',
-                    'action':buy_ticket_action
-                },
-            ]
-        }
-    ]
-}
-
-"""
-def generate_menu_from_dict(dict, parent=None):
-    """
-    Returns root (MenuNode) object creating children menu nodes
-    """
-    pass
