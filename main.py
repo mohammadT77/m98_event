96708e884414122f380a21f2f18b8892ab92ca95
matehrani <t.mohammadamin@gmail.com>
2023-05-19 10:40:27 +0200

    impl ticketing.models.py

--- main.py
+++ main.py
@@ -1,36 +1,49 @@
-from menu.models import generate_menu_from_dict
+# from menu.models import generate_menu_from_dict
 
 
-main_menu_dict = {
-    'name':'Maktab events',
-    'children':[
-        {
-            'name':'Login as manager',
-            'action': lambda: None,
-        },
-        {
-            'name': 'Events',
-            'children': [
-                {
-                    'name':'See al events',
-                    'action': lambda: None,       
-                },
-                {
-                    'name':'See an event\'s details',
-                    'action': lambda: None,
-                },
-                {
-                    'name': 'Add new event',
-                    'action': lambda: None,
-                }
-            ]
-        },
-        {
-            'name': 'Tickets',
-            'description':"See all Tickets",
-        }
-    ]
-}
+# main_menu_dict = {
+#     'name':'Maktab events',
+#     'children':[
+#         {
+#             'name':'Login as manager',
+#             'action': lambda: None,
+#         },
+#         {
+#             'name': 'Events',
+#             'children': [
+#                 {
+#                     'name':'See al events',
+#                     'action': lambda: None,       
+#                 },
+#                 {
+#                     'name':'See an event\'s details',
+#                     'action': lambda: None,
+#                 },
+#                 {
+#                     'name': 'Add new event',
+#                     'action': lambda: None,
+#                 }
+#             ]
+#         },
+#         {
+#             'name': 'Tickets',
+#             'description':"See all Tickets",
+#         }
+#     ]
+# }
 
-root_menu = generate_menu_from_dict(main_menu_dict, parent=None)
-root_menu.print_tree()
+# root_menu = generate_menu_from_dict(main_menu_dict, parent=None)
+# root_menu.print_tree()
+
+from ticketing.models import Event
+from datetime import datetime
+# e1 = Event(1, "asd", 10, datetime.now())
+
+# e1.save_file()
+
+with open('1.event', 'r') as f:
+    e1 = Event.from_file_content(f.read())
+
+print(e1)
+print(e1.title)
+print(e1.event_date)
--- ticketing/models.py
+++ ticketing/models.py
@@ -1,8 +1,44 @@
+from datetime import datetime
+# from dataclasses import dataclass
+
+
 class ManagerUser:
     pass
 
 class Event:
-    pass
+    id: int
+    title: str
+    capacity: int
+    # reserved: int
+    event_date: datetime
+
+    def __init__(self, id, title, capacity, event_date) -> None:
+        self.id = id
+        self.title = title
+        self.capacity = capacity
+        self.event_date = event_date
+
+    
+    def save_file(self):
+        with open(self.get_file_path(), 'wt') as f:
+            f.write(self.get_file_content())
+
+    def get_file_path(self):
+        return f'{self.id}.event'
+
+
+    def get_file_content(self) -> str:
+        return f"{self.id}||{self.title}||{self.capacity}||{self.event_date}"
+
+    @classmethod
+    def from_file_content(cls, s: str):
+        # s = 1||akbar||10||20222.22.2.2.2.
+        # [1, akbar, 10, 22....]
+        id, title, cap, event_date = s.split('||')
+        return cls(int(id), title, int(cap), datetime.fromisoformat(event_date))
+
+
+    
 
 class Ticket:
     pass
